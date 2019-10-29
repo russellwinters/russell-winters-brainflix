30a5e9d9cab8af695759c91773db4feb02036027
russ_winters <russellwinters@protonmail.com>
2019-10-29 08:59:37 -0700

    Made and styled post information component for all sizes. Few minor details to check up on.

--- sprint-1/src/App.js
+++ sprint-1/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import MainVideo from "./components/Video";
+import VideoInfo from "./components/VideoInfo";
 
 function App() {
   return (
     <div>
       <Header />
       <MainVideo />
+      <VideoInfo />
     </div>
   );
 }
