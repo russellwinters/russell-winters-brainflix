ce07dc2f3239f6a415eb5b8baed52b27ffdd7444
russ_winters <russellwinters@protonmail.com>
2019-11-08 09:46:04 -0800

    Started with posting the comments

--- sprint-2/src/components/CommentSection.js
+++ sprint-2/src/components/CommentSection.js
@@ -2,49 +2,58 @@ import React from "react";
 import Headshot from "../assets/images/Mohan-muruge.jpg";
 import CommentBuilder from "./CommentBuilder";
 
-export default function CommentSection(props) {
-  let newComment = props.CommentData.comments;
-  let comments = newComment.map(comment => {
+export default class CommentSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      commentArr: props.CommentData.comments
+    };
+  }
+
+  render() {
+    let newComment = this.state.commentArr;
+    let comments = newComment.map(comment => {
+      return (
+        <CommentBuilder
+          timestamp={comment.timestamp}
+          name={comment.name}
+          comment={comment.comment}
+          key={comment.id}
+        />
+      );
+    });
     return (
-      <CommentBuilder
-        timestamp={comment.timestamp}
-        name={comment.name}
-        comment={comment.comment}
-        key={comment.id}
-      />
-    );
-  });
-  return (
-    <section className="comments">
-      <div className="comments-form">
-        <h3 className="comments-form__heading">
-          3{/* The 3 will be props.propName.length*/} Comments
-        </h3>
-        <div className="comments-form__container">
-          <img
-            className="comments-form__container-img"
-            src={Headshot}
-            alt="Headshot"
-          />
-          <div className="comments-form__container-subdiv">
-            <div className="comments-form__container-subdiv-input">
-              <span className="comments-form__container-subdiv-input--heading">
-                Join the Conversation
-              </span>
-              <textarea
-                className="comments-form__container-subdiv-input--content"
-                type="text"
-                placeholder="That was easily the most spectacular BMX moment ever."
-                resize="none"
-              ></textarea>
+      <section className="comments">
+        <div className="comments-form">
+          <h3 className="comments-form__heading">
+            3{/* The 3 will be props.propName.length*/} Comments
+          </h3>
+          <div className="comments-form__container">
+            <img
+              className="comments-form__container-img"
+              src={Headshot}
+              alt="Headshot"
+            />
+            <div className="comments-form__container-subdiv">
+              <form className="comments-form__container-subdiv-input">
+                <span className="comments-form__container-subdiv-input--heading">
+                  Join the Conversation
+                </span>
+                <textarea
+                  className="comments-form__container-subdiv-input--content"
+                  type="text"
+                  placeholder="That was easily the most spectacular BMX moment ever."
+                  resize="none"
+                ></textarea>
+              </form>
+              <button className="comments-form__container-subdiv-button">
+                Comment
+              </button>
             </div>
-            <button className="comments-form__container-subdiv-button">
-              Comment
-            </button>
           </div>
         </div>
-      </div>
-      {comments}
-    </section>
-  );
+        {comments}
+      </section>
+    );
+  }
 }
--- sprint-2/src/notes.md
+++ sprint-2/src/notes.md
@@ -1,5 +1,11 @@
 To do:
 
-4. Change Upload button link and styling based on link around button.
-5. Upload videos page
-6. Diving deeper: Post comments
+1. Change state in CommentSection to that prop of the comments, then create a function that's going to post to API then re-fetch the data, set state to the updated APi comments. Also will have to change the number of comments to that property.length.
+2. Change Upload button link and styling based on link around button.
+3. Upload videos page
+4. Diving deeper: Post comments
+
+Progress on Diving Deeper:
+
+1. Comment Section is now class component.
+2. Need to make onCLick function for form
